e1b4e4056dc6413cd434c3c6bf10c69b9555b530
One Seven <x@OnedeMacBook-Pro.local>
2017-07-17 09:01:17 +0800

    add Rotate Array

---
+++ algorithms/rotate.js
@@ -0,0 +1,15 @@
+/**
+Rotate an array of n elements to the right by k steps.
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+**/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function(nums, k) {
+    k = k % nums.length
+    // nums.splice(0, 0, ...nums.splice(-k, k))
+    nums.unshift(...nums.splice(-k, k))
+};
